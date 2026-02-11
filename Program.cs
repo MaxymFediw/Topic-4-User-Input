bb2a7c7ed15ef5b1d20505e7bae99feb4608cfb8
Maxym Fediw <362706764@GoTVDSB.ca>
2026-02-11 12:25:38 -0500

    checkpoint 1

--- Program.cs
+++ Program.cs
@@ -6,10 +6,34 @@
         {
             Console.WriteLine("Hello, World!");
 
+            Console.WriteLine();
 
+            Console.WriteLine("Press ENTER To Continue");
+            string username;
 
+            username = Console.ReadLine();
 
+            Console.Write("Enter Username:");
+            username = Console.ReadLine();
 
+            
+            Console.WriteLine("You're Username is: " + username + ".");
+            Console.WriteLine(); 
+           Console.WriteLine("Press ENTER To Continue");
+            Console.WriteLine("");
+            Console.ReadLine();
+
+            
+
+           string age;
+
+            age= Console.ReadLine();
+
+            Console.Write("Enter age: ");
+
+            age= Console.ReadLine();
+            Console.WriteLine("You're age is: " + age + ".");
+            Console.ReadLine();
 
 
 
